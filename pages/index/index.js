1140ba1e4d3d9546862970d9a08096e3cc6ebdd8
Xianfeng <jiaoxianfeng_0509@buaa.edu.cn>
2019-06-02 11:55:18 +0800

    edit something

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,33 @@
+var app = getApp()
 Page({
   data: {
     value: '1',
     checked: true,
     userID: 16721084,
     userTask: '前端开发',
+    id:app.globalData.userID
   },
-  sliderChange(e) {
-    this.setData({
-      slider: e.detail.value,
-    })
-  },
-  bindchange(e) {
-    console.log(e)
-  },
+
+    onLoad: function () {
+        var that = this
+        
+        wx.request({
+          url: 'http://114.115.151.96:8080/user/get',
+          data: {
+            id:1
+          },
+          method: 'GET',
+          success: function (res) {
+                //将获取到的json数据，存在名字叫list的这个数组中
+                that.setData({
+                    list: res.data,
+                    userID: res.data.User.userId
+                    //res代表success函数的事件对，data是固定的，list是数组
+                })
+                console.log(res.data);// 控制台显示 1
+            }
+        })
+
+
+    }
 })
